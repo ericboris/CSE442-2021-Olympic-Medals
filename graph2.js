0f94d82aeb422de6fb1beeef4aa57423fad345db
Eric Boris <ericboris@tutanota.com>
2021-11-01 12:19:18 -0700

    Display olympic data
    Include olympic data directly in graph2.js as json.
    Directly modify olympic data json headers, i.e. col1 -> rank, col2 -> country..
    Update references from DUMMY_DATA to DATA.

--- graph2.js
+++ graph2.js
@@ -1,4 +1,843 @@
 const DATA = [
+{
+	"rank": "1",
+	"country": "United States of America",
+	"gold": "39",
+	"silver": "41",
+	"bronze": "33",
+	"total": "113",
+	"rankTotal": "1"
+},
+{
+	"rank": "2",
+	"country": "People's Republic of China",
+	"gold": "38",
+	"silver": "32",
+	"bronze": "18",
+	"total": "88",
+	"rankTotal": "2"
+},
+{
+	"rank": "3",
+	"country": "Japan",
+	"gold": "27",
+	"silver": "14",
+	"bronze": "17",
+	"total": "58",
+	"rankTotal": "5"
+},
+{
+	"rank": "4",
+	"country": "Great Britain",
+	"gold": "22",
+	"silver": "21",
+	"bronze": "22",
+	"total": "65",
+	"rankTotal": "4"
+},
+{
+	"rank": "5",
+	"country": "ROC",
+	"gold": "20",
+	"silver": "28",
+	"bronze": "23",
+	"total": "71",
+	"rankTotal": "3"
+},
+{
+	"rank": "6",
+	"country": "Australia",
+	"gold": "17",
+	"silver": "7",
+	"bronze": "22",
+	"total": "46",
+	"rankTotal": "6"
+},
+{
+	"rank": "7",
+	"country": "Netherlands",
+	"gold": "10",
+	"silver": "12",
+	"bronze": "14",
+	"total": "36",
+	"rankTotal": "9"
+},
+{
+	"rank": "8",
+	"country": "France",
+	"gold": "10",
+	"silver": "12",
+	"bronze": "11",
+	"total": "33",
+	"rankTotal": "10"
+},
+{
+	"rank": "9",
+	"country": "Germany",
+	"gold": "10",
+	"silver": "11",
+	"bronze": "16",
+	"total": "37",
+	"rankTotal": "8"
+},
+{
+	"rank": "10",
+	"country": "Italy",
+	"gold": "10",
+	"silver": "10",
+	"bronze": "20",
+	"total": "40",
+	"rankTotal": "7"
+},
+{
+	"rank": "11",
+	"country": "Canada",
+	"gold": "7",
+	"silver": "6",
+	"bronze": "11",
+	"total": "24",
+	"rankTotal": "11"
+},
+{
+	"rank": "12",
+	"country": "Brazil",
+	"gold": "7",
+	"silver": "6",
+	"bronze": "8",
+	"total": "21",
+	"rankTotal": "12"
+},
+{
+	"rank": "13",
+	"country": "New Zealand",
+	"gold": "7",
+	"silver": "6",
+	"bronze": "7",
+	"total": "20",
+	"rankTotal": "13"
+},
+{
+	"rank": "14",
+	"country": "Cuba",
+	"gold": "7",
+	"silver": "3",
+	"bronze": "5",
+	"total": "15",
+	"rankTotal": "18"
+},
+{
+	"rank": "15",
+	"country": "Hungary",
+	"gold": "6",
+	"silver": "7",
+	"bronze": "7",
+	"total": "20",
+	"rankTotal": "13"
+},
+{
+	"rank": "16",
+	"country": "Republic of Korea",
+	"gold": "6",
+	"silver": "4",
+	"bronze": "10",
+	"total": "20",
+	"rankTotal": "13"
+},
+{
+	"rank": "17",
+	"country": "Poland",
+	"gold": "4",
+	"silver": "5",
+	"bronze": "5",
+	"total": "14",
+	"rankTotal": "19"
+},
+{
+	"rank": "18",
+	"country": "Czech Republic",
+	"gold": "4",
+	"silver": "4",
+	"bronze": "3",
+	"total": "11",
+	"rankTotal": "23"
+},
+{
+	"rank": "19",
+	"country": "Kenya",
+	"gold": "4",
+	"silver": "4",
+	"bronze": "2",
+	"total": "10",
+	"rankTotal": "25"
+},
+{
+	"rank": "20",
+	"country": "Norway",
+	"gold": "4",
+	"silver": "2",
+	"bronze": "2",
+	"total": "8",
+	"rankTotal": "29"
+},
+{
+	"rank": "21",
+	"country": "Jamaica",
+	"gold": "4",
+	"silver": "1",
+	"bronze": "4",
+	"total": "9",
+	"rankTotal": "26"
+},
+{
+	"rank": "22",
+	"country": "Spain",
+	"gold": "3",
+	"silver": "8",
+	"bronze": "6",
+	"total": "17",
+	"rankTotal": "17"
+},
+{
+	"rank": "23",
+	"country": "Sweden",
+	"gold": "3",
+	"silver": "6",
+	"bronze": "0",
+	"total": "9",
+	"rankTotal": "26"
+},
+{
+	"rank": "24",
+	"country": "Switzerland",
+	"gold": "3",
+	"silver": "4",
+	"bronze": "6",
+	"total": "13",
+	"rankTotal": "20"
+},
+{
+	"rank": "25",
+	"country": "Denmark",
+	"gold": "3",
+	"silver": "4",
+	"bronze": "4",
+	"total": "11",
+	"rankTotal": "23"
+},
+{
+	"rank": "26",
+	"country": "Croatia",
+	"gold": "3",
+	"silver": "3",
+	"bronze": "2",
+	"total": "8",
+	"rankTotal": "29"
+},
+{
+	"rank": "27",
+	"country": "Islamic Republic of Iran",
+	"gold": "3",
+	"silver": "2",
+	"bronze": "2",
+	"total": "7",
+	"rankTotal": "33"
+},
+{
+	"rank": "28",
+	"country": "Serbia",
+	"gold": "3",
+	"silver": "1",
+	"bronze": "5",
+	"total": "9",
+	"rankTotal": "26"
+},
+{
+	"rank": "29",
+	"country": "Belgium",
+	"gold": "3",
+	"silver": "1",
+	"bronze": "3",
+	"total": "7",
+	"rankTotal": "33"
+},
+{
+	"rank": "30",
+	"country": "Bulgaria",
+	"gold": "3",
+	"silver": "1",
+	"bronze": "2",
+	"total": "6",
+	"rankTotal": "39"
+},
+{
+	"rank": "31",
+	"country": "Slovenia",
+	"gold": "3",
+	"silver": "1",
+	"bronze": "1",
+	"total": "5",
+	"rankTotal": "42"
+},
+{
+	"rank": "32",
+	"country": "Uzbekistan",
+	"gold": "3",
+	"silver": "0",
+	"bronze": "2",
+	"total": "5",
+	"rankTotal": "42"
+},
+{
+	"rank": "33",
+	"country": "Georgia",
+	"gold": "2",
+	"silver": "5",
+	"bronze": "1",
+	"total": "8",
+	"rankTotal": "29"
+},
+{
+	"rank": "34",
+	"country": "Chinese Taipei",
+	"gold": "2",
+	"silver": "4",
+	"bronze": "6",
+	"total": "12",
+	"rankTotal": "22"
+},
+{
+	"rank": "35",
+	"country": "Turkey",
+	"gold": "2",
+	"silver": "2",
+	"bronze": "9",
+	"total": "13",
+	"rankTotal": "20"
+},
+{
+	"rank": "36",
+	"country": "Greece",
+	"gold": "2",
+	"silver": "1",
+	"bronze": "1",
+	"total": "4",
+	"rankTotal": "47"
+},
+{
+	"rank": "36",
+	"country": "Uganda",
+	"gold": "2",
+	"silver": "1",
+	"bronze": "1",
+	"total": "4",
+	"rankTotal": "47"
+},
+{
+	"rank": "38",
+	"country": "Ecuador",
+	"gold": "2",
+	"silver": "1",
+	"bronze": "0",
+	"total": "3",
+	"rankTotal": "60"
+},
+{
+	"rank": "39",
+	"country": "Ireland",
+	"gold": "2",
+	"silver": "0",
+	"bronze": "2",
+	"total": "4",
+	"rankTotal": "47"
+},
+{
+	"rank": "39",
+	"country": "Israel",
+	"gold": "2",
+	"silver": "0",
+	"bronze": "2",
+	"total": "4",
+	"rankTotal": "47"
+},
+{
+	"rank": "41",
+	"country": "Qatar",
+	"gold": "2",
+	"silver": "0",
+	"bronze": "1",
+	"total": "3",
+	"rankTotal": "60"
+},
+{
+	"rank": "42",
+	"country": "Bahamas",
+	"gold": "2",
+	"silver": "0",
+	"bronze": "0",
+	"total": "2",
+	"rankTotal": "66"
+},
+{
+	"rank": "42",
+	"country": "Kosovo",
+	"gold": "2",
+	"silver": "0",
+	"bronze": "0",
+	"total": "2",
+	"rankTotal": "66"
+},
+{
+	"rank": "44",
+	"country": "Ukraine",
+	"gold": "1",
+	"silver": "6",
+	"bronze": "12",
+	"total": "19",
+	"rankTotal": "16"
+},
+{
+	"rank": "45",
+	"country": "Belarus",
+	"gold": "1",
+	"silver": "3",
+	"bronze": "3",
+	"total": "7",
+	"rankTotal": "33"
+},
+{
+	"rank": "46",
+	"country": "Romania",
+	"gold": "1",
+	"silver": "3",
+	"bronze": "0",
+	"total": "4",
+	"rankTotal": "47"
+},
+{
+	"rank": "46",
+	"country": "Venezuela",
+	"gold": "1",
+	"silver": "3",
+	"bronze": "0",
+	"total": "4",
+	"rankTotal": "47"
+},
+{
+	"rank": "48",
+	"country": "India",
+	"gold": "1",
+	"silver": "2",
+	"bronze": "4",
+	"total": "7",
+	"rankTotal": "33"
+},
+{
+	"rank": "49",
+	"country": "Hong Kong, China",
+	"gold": "1",
+	"silver": "2",
+	"bronze": "3",
+	"total": "6",
+	"rankTotal": "39"
+},
+{
+	"rank": "50",
+	"country": "Philippines",
+	"gold": "1",
+	"silver": "2",
+	"bronze": "1",
+	"total": "4",
+	"rankTotal": "47"
+},
+{
+	"rank": "50",
+	"country": "Slovakia",
+	"gold": "1",
+	"silver": "2",
+	"bronze": "1",
+	"total": "4",
+	"rankTotal": "47"
+},
+{
+	"rank": "52",
+	"country": "South Africa",
+	"gold": "1",
+	"silver": "2",
+	"bronze": "0",
+	"total": "3",
+	"rankTotal": "60"
+},
+{
+	"rank": "53",
+	"country": "Austria",
+	"gold": "1",
+	"silver": "1",
+	"bronze": "5",
+	"total": "7",
+	"rankTotal": "33"
+},
+{
+	"rank": "54",
+	"country": "Egypt",
+	"gold": "1",
+	"silver": "1",
+	"bronze": "4",
+	"total": "6",
+	"rankTotal": "39"
+},
+{
+	"rank": "55",
+	"country": "Indonesia",
+	"gold": "1",
+	"silver": "1",
+	"bronze": "3",
+	"total": "5",
+	"rankTotal": "42"
+},
+{
+	"rank": "56",
+	"country": "Ethiopia",
+	"gold": "1",
+	"silver": "1",
+	"bronze": "2",
+	"total": "4",
+	"rankTotal": "47"
+},
+{
+	"rank": "56",
+	"country": "Portugal",
+	"gold": "1",
+	"silver": "1",
+	"bronze": "2",
+	"total": "4",
+	"rankTotal": "47"
+},
+{
+	"rank": "58",
+	"country": "Tunisia",
+	"gold": "1",
+	"silver": "1",
+	"bronze": "0",
+	"total": "2",
+	"rankTotal": "66"
+},
+{
+	"rank": "59",
+	"country": "Estonia",
+	"gold": "1",
+	"silver": "0",
+	"bronze": "1",
+	"total": "2",
+	"rankTotal": "66"
+},
+{
+	"rank": "59",
+	"country": "Fiji",
+	"gold": "1",
+	"silver": "0",
+	"bronze": "1",
+	"total": "2",
+	"rankTotal": "66"
+},
+{
+	"rank": "59",
+	"country": "Latvia",
+	"gold": "1",
+	"silver": "0",
+	"bronze": "1",
+	"total": "2",
+	"rankTotal": "66"
+},
+{
+	"rank": "59",
+	"country": "Thailand",
+	"gold": "1",
+	"silver": "0",
+	"bronze": "1",
+	"total": "2",
+	"rankTotal": "66"
+},
+{
+	"rank": "63",
+	"country": "Bermuda",
+	"gold": "1",
+	"silver": "0",
+	"bronze": "0",
+	"total": "1",
+	"rankTotal": "77"
+},
+{
+	"rank": "63",
+	"country": "Morocco",
+	"gold": "1",
+	"silver": "0",
+	"bronze": "0",
+	"total": "1",
+	"rankTotal": "77"
+},
+{
+	"rank": "63",
+	"country": "Puerto Rico",
+	"gold": "1",
+	"silver": "0",
+	"bronze": "0",
+	"total": "1",
+	"rankTotal": "77"
+},
+{
+	"rank": "66",
+	"country": "Colombia",
+	"gold": "0",
+	"silver": "4",
+	"bronze": "1",
+	"total": "5",
+	"rankTotal": "42"
+},
+{
+	"rank": "67",
+	"country": "Azerbaijan",
+	"gold": "0",
+	"silver": "3",
+	"bronze": "4",
+	"total": "7",
+	"rankTotal": "33"
+},
+{
+	"rank": "68",
+	"country": "Dominican Republic",
+	"gold": "0",
+	"silver": "3",
+	"bronze": "2",
+	"total": "5",
+	"rankTotal": "42"
+},
+{
+	"rank": "69",
+	"country": "Armenia",
+	"gold": "0",
+	"silver": "2",
+	"bronze": "2",
+	"total": "4",
+	"rankTotal": "47"
+},
+{
+	"rank": "70",
+	"country": "Kyrgyzstan",
+	"gold": "0",
+	"silver": "2",
+	"bronze": "1",
+	"total": "3",
+	"rankTotal": "60"
+},
+{
+	"rank": "71",
+	"country": "Mongolia",
+	"gold": "0",
+	"silver": "1",
+	"bronze": "3",
+	"total": "4",
+	"rankTotal": "47"
+},
+{
+	"rank": "72",
+	"country": "Argentina",
+	"gold": "0",
+	"silver": "1",
+	"bronze": "2",
+	"total": "3",
+	"rankTotal": "60"
+},
+{
+	"rank": "72",
+	"country": "San Marino",
+	"gold": "0",
+	"silver": "1",
+	"bronze": "2",
+	"total": "3",
+	"rankTotal": "60"
+},
+{
+	"rank": "74",
+	"country": "Jordan",
+	"gold": "0",
+	"silver": "1",
+	"bronze": "1",
+	"total": "2",
+	"rankTotal": "66"
+},
+{
+	"rank": "74",
+	"country": "Malaysia",
+	"gold": "0",
+	"silver": "1",
+	"bronze": "1",
+	"total": "2",
+	"rankTotal": "66"
+},
+{
+	"rank": "74",
+	"country": "Nigeria",
+	"gold": "0",
+	"silver": "1",
+	"bronze": "1",
+	"total": "2",
+	"rankTotal": "66"
+},
+{
+	"rank": "77",
+	"country": "Bahrain",
+	"gold": "0",
+	"silver": "1",
+	"bronze": "0",
+	"total": "1",
+	"rankTotal": "77"
+},
+{
+	"rank": "77",
+	"country": "Saudi Arabia",
+	"gold": "0",
+	"silver": "1",
+	"bronze": "0",
+	"total": "1",
+	"rankTotal": "77"
+},
+{
+	"rank": "77",
+	"country": "Lithuania",
+	"gold": "0",
+	"silver": "1",
+	"bronze": "0",
+	"total": "1",
+	"rankTotal": "77"
+},
+{
+	"rank": "77",
+	"country": "North Macedonia",
+	"gold": "0",
+	"silver": "1",
+	"bronze": "0",
+	"total": "1",
+	"rankTotal": "77"
+},
+{
+	"rank": "77",
+	"country": "Namibia",
+	"gold": "0",
+	"silver": "1",
+	"bronze": "0",
+	"total": "1",
+	"rankTotal": "77"
+},
+{
+	"rank": "77",
+	"country": "Turkmenistan",
+	"gold": "0",
+	"silver": "1",
+	"bronze": "0",
+	"total": "1",
+	"rankTotal": "77"
+},
+{
+	"rank": "83",
+	"country": "Kazakhstan",
+	"gold": "0",
+	"silver": "0",
+	"bronze": "8",
+	"total": "8",
+	"rankTotal": "29"
+},
+{
+	"rank": "84",
+	"country": "Mexico",
+	"gold": "0",
+	"silver": "0",
+	"bronze": "4",
+	"total": "4",
+	"rankTotal": "47"
+},
+{
+	"rank": "85",
+	"country": "Finland",
+	"gold": "0",
+	"silver": "0",
+	"bronze": "2",
+	"total": "2",
+	"rankTotal": "66"
+},
+{
+	"rank": "86",
+	"country": "Botswana",
+	"gold": "0",
+	"silver": "0",
+	"bronze": "1",
+	"total": "1",
+	"rankTotal": "77"
+},
+{
+	"rank": "86",
+	"country": "Burkina Faso",
+	"gold": "0",
+	"silver": "0",
+	"bronze": "1",
+	"total": "1",
+	"rankTotal": "77"
+},
+{
+	"rank": "86",
+	"country": "Côte d'Ivoire",
+	"gold": "0",
+	"silver": "0",
+	"bronze": "1",
+	"total": "1",
+	"rankTotal": "77"
+},
+{
+	"rank": "86",
+	"country": "Ghana",
+	"gold": "0",
+	"silver": "0",
+	"bronze": "1",
+	"total": "1",
+	"rankTotal": "77"
+},
+{
+	"rank": "86",
+	"country": "Grenada",
+	"gold": "0",
+	"silver": "0",
+	"bronze": "1",
+	"total": "1",
+	"rankTotal": "77"
+},
+{
+	"rank": "86",
+	"country": "Kuwait",
+	"gold": "0",
+	"silver": "0",
+	"bronze": "1",
+	"total": "1",
+	"rankTotal": "77"
+},
+{
+	"rank": "86",
+	"country": "Republic of Moldova",
+	"gold": "0",
+	"silver": "0",
+	"bronze": "1",
+	"total": "1",
+	"rankTotal": "77"
+},
+{
+	"rank": "86",
+	"country": "Syrian Arab Republic",
+	"gold": "0",
+	"silver": "0",
+	"bronze": "1",
+	"total": "1",
+	"rankTotal": "77"
+}];
+
+const DUMMY_DATA = [
     { id: 'd1', country: 'Nepal', value: 10 },
     { id: 'd2', country: 'Thailand', value: 14 },
     { id: 'd3', country: 'Vietnam', value: 16 },
@@ -17,7 +856,7 @@ const chartContainer = d3
     .attr('height', CHART_HEIGHT + MARGINS.top + MARGINS.bottom);
 
 // Minimum space between top of tallest bar and top of graph.
-const HEADSPACE = 2;
+const HEADSPACE = 8;
 
 // Horizontal padding between bars.
 const BAR_PADDING = 0.1;
@@ -33,7 +872,7 @@ const x = d3
 const y = d3
     .scaleLinear()
     .range([CHART_HEIGHT, 0])
-    .domain([0, d3.max(DATA, (d) => d.value) + HEADSPACE]);
+    .domain([0, d3.max(DATA, (d) => d.total) + HEADSPACE]);
 
 const chart = chartContainer.append('g');
 
@@ -55,38 +894,38 @@ function renderChart() {
     // Show selected chart bars.
     chart
         .selectAll('.bar')
-        .data(selectedData, d => d.id)
+        .data(selectedData, d => d.rank)
         .enter()
         .append('rect')
         .classed('bar', true)
         .attr('width', x.bandwidth())
-        .attr('height', (d) => CHART_HEIGHT - y(d.value))
+        .attr('height', (d) => CHART_HEIGHT - y(d.total))
         .attr('x', (d) => x(d.country))
-        .attr('y', (d) => y(d.value));
+        .attr('y', (d) => y(d.total));
 
     // Show selected bar value labels.
     chart
         .selectAll('.label')
-        .data(selectedData, d => d.id)
+        .data(selectedData, d => d.rank)
         .enter()
         .append('text')
-        .text((d) => d.value)
+        .text((d) => d.total)
         .attr('x',(d)=> x(d.country) + VALUE_LABEL_HORIZ_OFFSET)
-        .attr('y',(d)=> y(d.value) + VALUE_LABEL_VERT_OFFSET)
+        .attr('y',(d)=> y(d.total) + VALUE_LABEL_VERT_OFFSET)
         .attr('text-anchor', 'middle')
         .classed('label', true);
 
     // Remove unselected chart bars.
     chart
         .selectAll('.bar')
-        .data(selectedData, d => d.id)
+        .data(selectedData, d => d.rank)
         .exit()
         .remove();
 
     // Remove unselected chart labels.
     chart
         .selectAll('.label')
-        .data(selectedData, d => d.id)
+        .data(selectedData, d => d.rank)
         .exit()
         .remove();
 }
@@ -115,12 +954,12 @@ listItems
     .attr('type', 'checkbox')
     .attr('checked', true)
     .on('change', (d1) => {
-        if (unselectedIds.indexOf(d1.id) === -1) {
-            unselectedIds.push(d1.id);
+        if (unselectedIds.indexOf(d1.rank) === -1) {
+            unselectedIds.push(d1.rank);
         } else {
-            unselectedIds = unselectedIds.filter((id) => id !== d1.id);
+            unselectedIds = unselectedIds.filter((id) => id !== d1.rank);
         }
         // Update selected data based on current selection.
-        selectedData = DATA.filter((d2) => unselectedIds.indexOf(d2.id) === -1);
+        selectedData = DATA.filter((d2) => unselectedIds.indexOf(d2.rank) === -1);
         renderChart();
     });
