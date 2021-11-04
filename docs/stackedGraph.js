eae656d0e6f5d63c53f56b241e2d2d5585fc74e6
Eric Boris <ericboris@tutanota.com>
2021-11-04 12:23:03 -0700

    Implement stacked bar chart
    Add new stacked bar chart file,
    modify it to work with olympic data,
    and, update index.html to cooperate with this new file.

--- docs/stackedGraph.js
+++ docs/stackedGraph.js
@@ -1,7 +1,7 @@
-const DATA = [
+const METALS_DATA = [
 	{
 		"rank": "1",
-		"country": "USA",
+		"country": "United States of America",
 		"gold": "39",
 		"silver": "41",
 		"bronze": "33",
@@ -10,7 +10,7 @@ const DATA = [
 	},
 	{
 		"rank": "2",
-		"country": "China",
+		"country": "People's Republic of China",
 		"gold": "38",
 		"silver": "32",
 		"bronze": "18",
@@ -136,7 +136,7 @@ const DATA = [
 	},
 	{
 		"rank": "16",
-		"country": "Korea",
+		"country": "Republic of Korea",
 		"gold": "6",
 		"silver": "4",
 		"bronze": "10",
@@ -235,7 +235,7 @@ const DATA = [
 	},
 	{
 		"rank": "27",
-		"country": "Iran",
+		"country": "Islamic Republic of Iran",
 		"gold": "3",
 		"silver": "2",
 		"bronze": "2",
@@ -298,7 +298,7 @@ const DATA = [
 	},
 	{
 		"rank": "34",
-		"country": "Taipei",
+		"country": "Chinese Taipei",
 		"gold": "2",
 		"silver": "4",
 		"bronze": "6",
@@ -433,7 +433,7 @@ const DATA = [
 	},
 	{
 		"rank": "49",
-		"country": "Hong Kong",
+		"country": "Hong Kong, China",
 		"gold": "1",
 		"silver": "2",
 		"bronze": "3",
@@ -712,7 +712,7 @@ const DATA = [
 	},
 	{
 		"rank": "77",
-		"country": "Macedonia",
+		"country": "North Macedonia",
 		"gold": "0",
 		"silver": "1",
 		"bronze": "0",
@@ -820,7 +820,7 @@ const DATA = [
 	},
 	{
 		"rank": "86",
-		"country": "Moldova",
+		"country": "Republic of Moldova",
 		"gold": "0",
 		"silver": "0",
 		"bronze": "1",
@@ -829,7 +829,7 @@ const DATA = [
 	},
 	{
 		"rank": "86",
-		"country": "Syria",
+		"country": "Syrian Arab Republic",
 		"gold": "0",
 		"silver": "0",
 		"bronze": "1",
@@ -837,249 +837,293 @@ const DATA = [
 		"rankTotal": "77"
 	}
 ];
-	
-// import {Data} from './BetterMedals.json'
-// const DATA = Data
 
-//import {scrubber} from "";
+medals = ['gold', 'silver', 'bronze'];
 
+countryMedals = medals.flatMap(medal => METALS_DATA.map(d => ({country: d.country, medal, count: d[medal]}))) // pivot longer
 
-// Chart dimensions.
-// Use margins to keep axes within frame.
-const MARGINS = {top: 20, bottom: 10};
-const CHART_WIDTH = 2600;
-const CHART_HEIGHT = 700 - MARGINS.top - MARGINS.bottom;
+chart = StackedBarChart(countryMedals, {
+  x: d => d.country,
+  y: d => d.count,
+  z: d => d.medal,
+  xDomain: d3.groupSort(countryMedals, D => d3.sum(D, d => -d.count), d => d.country),
+  yLabel: "↑ Total Medals Earned",
+  zDomain: medals,
+  //colors: d3.schemeSpectral[medals.length],
+  colors: ["#ffd700", "#c0c0c0", "#b08d57"],
+  width: 800,
+  height: 500
+})
 
-const chartContainer = d3
+key = Legend(chart.scales.color, {title: "Medal (color)"});
+
+// Copyright 2021 Observable, Inc.
+// Released under the ISC license.
+// https://observablehq.com/@d3/stacked-bar-chart
+function StackedBarChart(data, {
+  x = (d, i) => i, // given d in data, returns the (ordinal) x-value
+  y = d => d, // given d in data, returns the (quantitative) y-value
+  z = () => 1, // given d in data, returns the (categorical) z-value
+  title, // given d in data, returns the title text
+  marginTop = 30, // top margin, in pixels
+  marginRight = 0, // right margin, in pixels
+  marginBottom = 30, // bottom margin, in pixels
+  marginLeft = 40, // left margin, in pixels
+  width = 640, // outer width, in pixels
+  height = 400, // outer height, in pixels
+  xDomain, // array of x-values
+  xRange = [marginLeft, width - marginRight], // [left, right]
+  xPadding = 0.1, // amount of x-range to reserve to separate bars
+  yType = d3.scaleLinear, // type of y-scale
+  yDomain, // [ymin, ymax]
+  yRange = [height - marginBottom, marginTop], // [bottom, top]
+  zDomain, // array of z-values
+  offset = d3.stackOffsetDiverging, // stack offset method
+  order = d3.stackOrderNone, // stack order method
+  yFormat, // a format specifier string for the y-axis
+  yLabel, // a label for the y-axis
+  colors = d3.schemeTableau10, // array of colors
+} = {}) {
+  // Compute values.
+  const X = d3.map(data, x);
+  const Y = d3.map(data, y);
+  const Z = d3.map(data, z);
+
+  // Compute default x- and z-domains, and unique them.
+  if (xDomain === undefined) xDomain = X;
+  if (zDomain === undefined) zDomain = Z;
+  xDomain = new d3.InternSet(xDomain);
+  zDomain = new d3.InternSet(zDomain);
+
+  // Omit any data not present in the x- and z-domains.
+  const I = d3.range(X.length).filter(i => xDomain.has(X[i]) && zDomain.has(Z[i]));
+
+  // Compute a nested array of series where each series is [[y1, y2], [y1, y2],
+  // [y1, y2], …] representing the y-extent of each stacked rect. In addition,
+  // each tuple has an i (index) property so that we can refer back to the
+  // original data point (data[i]). This code assumes that there is only one
+  // data point for a given unique x- and z-value.
+  const series = d3.stack()
+      .keys(zDomain)
+      .value(([x, I], z) => Y[I.get(z)])
+      .order(order)
+      .offset(offset)
+    (d3.rollup(I, ([i]) => i, i => X[i], i => Z[i]))
+    .map(s => s.map(d => Object.assign(d, {i: d.data[1].get(s.key)})));
+
+  // Compute the default y-domain. Note: diverging stacks can be negative.
+  if (yDomain === undefined) yDomain = d3.extent(series.flat(2));
+
+  // Construct scales, axes, and formats.
+  const xScale = d3.scaleBand(xDomain, xRange).paddingInner(xPadding);
+  const yScale = yType(yDomain, yRange);
+  const color = d3.scaleOrdinal(zDomain, colors);
+  const xAxis = d3.axisBottom(xScale).tickSizeOuter(0);
+  const yAxis = d3.axisLeft(yScale).ticks(height / 60, yFormat);
+
+  // Compute titles.
+  if (title === undefined) {
+    const formatValue = yScale.tickFormat(100, yFormat);
+    title = i => `${X[i]}\n${Z[i]}\n${formatValue(Y[i])}`;
+  } else {
+    const O = d3.map(data, d => d);
+    const T = title;
+    title = i => T(O[i], i, data);
+  }
+
+  const svg = d3
+    .select("#chart")
+    .select("svg")
+      .attr("width", width)
+      .attr("height", height)
+      .attr("viewBox", [0, 0, width, height])
+      .attr("style", "max-width: 100%; height: auto; height: intrinsic;");
+
+  svg.append("g")
+      .attr("transform", `translate(${marginLeft},0)`)
+      .call(yAxis)
+      .call(g => g.select(".domain").remove())
+      .call(g => g.selectAll(".tick line").clone()
+          .attr("x2", width - marginLeft - marginRight)
+          .attr("stroke-opacity", 0.1))
+      .call(g => g.append("text")
+          .attr("x", -marginLeft)
+          .attr("y", 10)
+          .attr("fill", "currentColor")
+          .attr("text-anchor", "start")
+          .text(yLabel));
+
+  const bar = svg.append("g")
+    .selectAll("g")
+    .data(series)
+    .join("g")
+      .attr("fill", ([{i}]) => color(Z[i]))
+    .selectAll("rect")
+    .data(d => d)
+    .join("rect")
+      .attr("x", ({i}) => xScale(X[i]))
+      .attr("y", ([y1, y2]) => Math.min(yScale(y1), yScale(y2)))
+      .attr("height", ([y1, y2]) => Math.abs(yScale(y1) - yScale(y2)))
+      .attr("width", xScale.bandwidth());
+
+  if (title) bar.append("title")
+      .text(({i}) => title(i));
+
+  svg.append("g")
+      .attr("transform", `translate(0,${yScale(0)})`)
+      .call(xAxis);
+
+  return Object.assign(svg.node(), {scales: {color}});
+}
+
+// Copyright 2021, Observable Inc.
+// Released under the ISC license.
+// https://observablehq.com/@d3/color-legend
+function Legend(color, {
+  title,
+  tickSize = 6,
+  width = 320,
+  height = 44 + tickSize,
+  marginTop = 18,
+  marginRight = 0,
+  marginBottom = 16 + tickSize,
+  marginLeft = 0,
+  ticks = width / 64,
+  tickFormat,
+  tickValues
+} = {}) {
+
+  function ramp(color, n = 256) {
+    const canvas = document.createElement("canvas");
+    canvas.width = n;
+    canvas.height = 1;
+    const context = canvas.getContext("2d");
+    for (let i = 0; i < n; ++i) {
+      context.fillStyle = color(i / (n - 1));
+      context.fillRect(i, 0, 1, 1);
+    }
+    return canvas;
+  }
+
+  const svg = d3
+    .select('#legend')
     .select('svg')
-    .attr('width', CHART_WIDTH)
-    .attr('height', CHART_HEIGHT + MARGINS.top + MARGINS.bottom);
+      .attr("width", width)
+      .attr("height", height)
+      .attr("viewBox", [0, 0, width, height])
+      .style("overflow", "visible")
+      .style("display", "block");
 
-// Minimum space between top of tallest bar and top of graph.
-const HEADSPACE = 8;
+  let tickAdjust = g => g.selectAll(".tick line").attr("y1", marginTop + marginBottom - height);
+  let x;
 
-// Horizontal padding between bars.
-const BAR_PADDING = 0.1;
+  // Continuous
+  if (color.interpolate) {
+    const n = Math.min(color.domain().length, color.range().length);
 
-// Define bar width.
-const x = d3
-    .scaleBand()
-    .rangeRound([0, CHART_WIDTH/2])
-    .padding(BAR_PADDING)
-    .domain(DATA.map((d) => d.country));
+    x = color.copy().rangeRound(d3.quantize(d3.interpolate(marginLeft, width - marginRight), n));
 
-// Define bar height.
-const y = d3
-    .scaleLinear()
-    .range([CHART_HEIGHT + HEADSPACE, MARGINS.top])
-    .domain([0, d3.max(DATA, (d) => d.total) + HEADSPACE])
-    .nice();
+    svg.append("image")
+        .attr("x", marginLeft)
+        .attr("y", marginTop)
+        .attr("width", width - marginLeft - marginRight)
+        .attr("height", height - marginTop - marginBottom)
+        .attr("preserveAspectRatio", "none")
+        .attr("xlink:href", ramp(color.copy().domain(d3.quantize(d3.interpolate(0, 1), n))).toDataURL());
+  }
 
-const charttotal = chartContainer.append('g');
+  // Sequential
+  else if (color.interpolator) {
+    x = Object.assign(color.copy()
+        .interpolator(d3.interpolateRound(marginLeft, width - marginRight)),
+        {range() { return [marginLeft, width - marginRight]; }});
 
-// Create x axis.
-charttotal
-    .append('g')
-    .attr('transform', `translate(0, ${CHART_HEIGHT})`)
-    .attr('color', 'black')
-    .call(d3.axisBottom(x).tickSizeOuter(0))
-    .append('text')
-      .attr('text-anchor', 'end')
-      .attr('fill', 'red')
-      .attr('font-size', '12px')
-      .attr('font-weight', 'bold')
-      .attr('x', CHART_WIDTH/2)
-      .attr('y', 28)
-      .text('Country');
+    svg.append("image")
+        .attr("x", marginLeft)
+        .attr("y", marginTop)
+        .attr("width", width - marginLeft - marginRight)
+        .attr("height", height - marginTop - marginBottom)
+        .attr("preserveAspectRatio", "none")
+        .attr("xlink:href", ramp(color.interpolator()).toDataURL());
 
-// Create y axis.
-charttotal
-    .append('g')
-    .attr('transform', `translate(30,0)`)
-    .attr('color', 'black')
-    .call(d3.axisLeft(y).tickSizeOuter(0))
-    .append('text')
-        .attr('transform', `translate(0, 10) rotate(-90)`)
-        .attr('text-anchor', 'end')
-        .attr('fill', 'red')
-        .attr('font-size', '12px')
-        .attr('font-weight', 'bold')
-        .text('Total Medals Earned');
-    
-// Bar value label offsets.
-const VALUE_LABEL_HORIZ_OFFSET = x.bandwidth() / 2;
-const VALUE_LABEL_VERT_OFFSET = -20;
+    // scaleSequentialQuantile doesn’t implement ticks or tickFormat.
+    if (!x.ticks) {
+      if (tickValues === undefined) {
+        const n = Math.round(ticks + 1);
+        tickValues = d3.range(n).map(i => d3.quantile(color.domain(), i / (n - 1)));
+      }
+      if (typeof tickFormat !== "function") {
+        tickFormat = d3.format(tickFormat === undefined ? ",f" : tickFormat);
+      }
+    }
+  }
 
-// Render only selected data.
-let selectedData = [];
+  // Threshold
+  else if (color.invertExtent) {
+    const thresholds
+        = color.thresholds ? color.thresholds() // scaleQuantize
+        : color.quantiles ? color.quantiles() // scaleQuantile
+        : color.domain(); // scaleThreshold
 
-function renderChart() {
-    // Show selected chart bars.
-    charttotal
-        .selectAll('.bar')
-        .data(selectedData, d => d.rank)
-        .enter()
-        .append('rect')
-        .classed('bar', true)
-        .attr('width', x.bandwidth())
-        .attr('height', (d) => CHART_HEIGHT - y(d.total))
-        .attr('x', (d) => x(d.country))
-        .attr('y', (d) => y(d.total));
+    const thresholdFormat
+        = tickFormat === undefined ? d => d
+        : typeof tickFormat === "string" ? d3.format(tickFormat)
+        : tickFormat;
 
-    // Show selected bar value labels.
-    charttotal
-        .selectAll('.label')
-        .data(selectedData, d => d.rank)
-        .enter()
-        .append('text')
-        .text((d) => d.total)
-        .attr('x',(d)=> x(d.country) + VALUE_LABEL_HORIZ_OFFSET)
-        .attr('y',(d)=> y(d.total) + VALUE_LABEL_VERT_OFFSET)
-        .attr('text-anchor', 'middle')
-        .classed('label', true);
+    x = d3.scaleLinear()
+        .domain([-1, color.range().length - 1])
+        .rangeRound([marginLeft, width - marginRight]);
 
-    // Remove unselected chart bars.
-    charttotal
-        .selectAll('.bar')
-        .data(selectedData, d => d.rank)
-        .exit()
-        .remove();
-    
-    // Remove unselected chart labels.
-    charttotal
-        .selectAll('.label')
-        .data(selectedData, d => d.rank)
-        .exit()
-        .remove();
+    svg.append("g")
+      .selectAll("rect")
+      .data(color.range())
+      .join("rect")
+        .attr("x", (d, i) => x(i - 1))
+        .attr("y", marginTop)
+        .attr("width", (d, i) => x(i) - x(i - 1))
+        .attr("height", height - marginTop - marginBottom)
+        .attr("fill", d => d);
+
+    tickValues = d3.range(thresholds.length);
+    tickFormat = i => thresholdFormat(thresholds[i], i);
+  }
+
+  // Ordinal
+  else {
+    x = d3.scaleBand()
+        .domain(color.domain())
+        .rangeRound([marginLeft, width - marginRight]);
+
+    svg.append("g")
+      .selectAll("rect")
+      .data(color.domain())
+      .join("rect")
+        .attr("x", x)
+        .attr("y", marginTop)
+        .attr("width", Math.max(0, x.bandwidth() - 1))
+        .attr("height", height - marginTop - marginBottom)
+        .attr("fill", color);
+
+    tickAdjust = () => {};
+  }
+
+  svg.append("g")
+      .attr("transform", `translate(0,${height - marginBottom})`)
+      .call(d3.axisBottom(x)
+        .ticks(ticks, typeof tickFormat === "string" ? tickFormat : undefined)
+        .tickFormat(typeof tickFormat === "function" ? tickFormat : undefined)
+        .tickSize(tickSize)
+        .tickValues(tickValues))
+      .call(tickAdjust)
+      .call(g => g.select(".domain").remove())
+      .call(g => g.append("text")
+        .attr("x", marginLeft)
+        .attr("y", marginTop + marginBottom - height - 6)
+        .attr("fill", "currentColor")
+        .attr("text-anchor", "start")
+        .attr("font-weight", "bold")
+        .attr("class", "title")
+        .text(title));
+
+  return svg.node();
 }
-
-// Initialize page with blank chart.
-renderChart();
-
-// Create empty list.
-const listItems = d3
-    .select('#data')
-    .classed('scrollCheckbox', true)
-    .select('ul')
-    .selectAll('li')
-    .data(DATA)
-    .enter()
-    .append('li');
-
-// Initialize checklist items.
-listItems
-    .append('span')
-    .text((d)=> d.country)
-    .append('input')
-    .attr('type', 'checkbox');
-
-// Initialize empty checklist.
-let unselectedIds = [];
-for (let i = 1; i <= DATA.length; i++) {
-    unselectedIds.push(i.toString());
-}
-
-// Add event listener to checklist items.
-listItems
-    .on('change', (d1) => {
-        if (unselectedIds.indexOf(d1.rank) === -1) {
-            unselectedIds.push(d1.rank);
-        } else {
-            unselectedIds = unselectedIds.filter((id) => id !== d1.rank);
-        }
-        // Update selected data based on current selection.
-        selectedData = DATA.filter((d2) => unselectedIds.indexOf(d2.rank) === -1);
-        renderChart();
-    });
-
-
-/* ---------------------- GOLD CHART TIME OUT ZONE ---------------------------
-
-//GOLD CHART
-function renderGoldChart() {
-    // Show gold chart bars.
-    chartgold
-        .selectAll('.bar')
-        .data(selectedData, d => d.rank)
-        .enter()
-        .append('rect')
-        .classed('bar', true)
-        .attr('width', x2.bandwidth())
-        .attr('height', (d) => CHART_HEIGHT - y(d.gold))
-        .attr('x', (d) => x(d.country))
-        .attr('y', (d) => y(d.gold));
-
-    // Show selected bar value labels.
-    chartgold
-        .selectAll('.label')
-        .data(selectedData, d => d.rank)
-        .enter()
-        .append('text')
-        .text((d) => d.gold)
-        .attr('x',(d)=> x(d.country) + VALUE_LABEL_HORIZ_OFFSET2)
-        .attr('y',(d)=> y(d.gold) + VALUE_LABEL_VERT_OFFSET2)
-        .attr('text-anchor', 'middle')
-        .classed('label', true);
-
-    // Remove unselected chart bars.
-    chartgold
-        .selectAll('.bar')
-        .data(selectedData, d => d.rank)
-        .exit()
-        .remove();
-
-    // Remove unselected chart labels.
-    chartgold
-        .selectAll('.label')
-        .data(selectedData, d => d.rank)
-        .exit()
-        .remove();
- 
-}
-
-//Define bar width.
-const x2 = d3
-    .scaleBand()
-    .rangeRound([CHART_WIDTH/2, CHART_WIDTH])
-    .padding(BAR_PADDING)
-    .domain(DATA.map((d) => d.country));
-// Define bar height.
-const y2 = d3
-    .scaleLinear()
-    .range([CHART_HEIGHT + HEADSPACE, MARGINS.top])
-    .domain([0, d3.max(DATA, (d) => d.gold) + HEADSPACE])
-    .nice();
-const chartgold = chartContainer.append('g');
-
-// Create chart axes.
-chartgold
-    .append('g')
-    .attr('transform', `translate(0, ${CHART_HEIGHT})`)
-    .attr('color', 'black')
-    .call(d3.axisBottom(x2).tickSizeOuter(0))
-    .append('text')
-      .attr('text-anchor', 'end')
-      .attr('fill', 'black')
-      .attr('font-size', '50px')
-      .attr('font-weight', 'bold')
-      .attr('x', CHART_WIDTH/2)
-      .attr('y', 28)
-      .text('Country');
-
-chartgold
-    .append('g')
-    .attr('transform', `translate(20,0)`)
-    .attr('color', 'black')
-    .call(d3.axisLeft(y2).tickSizeOuter(0))
-    .append('text')
-        .attr('transform', `translate(0, 10) rotate(-90)`)
-        .attr('text-anchor', 'end')
-        .attr('fill', 'black')
-        .attr('font-size', '12px')
-        .attr('font-weight', 'bold')
-        .text('Gold Medals Earned');
-
-// Bar value label offsets.
-const VALUE_LABEL_HORIZ_OFFSET2 = x2.bandwidth() / 2;
-const VALUE_LABEL_VERT_OFFSET2 = -20;
-*/
